bd1ab6dfb53b535aa4559b68f30441d3d63c1245
Cassia Silva <cassia.siilva09@gmail.com>
2022-06-07 22:27:13 -0300

    .

---
+++ android/app/src/main/kotlin/com/example/gifs_buscador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gifs_buscador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:gifs_buscador/main.dart';
+import 'package:flutter/material.dart';
+import 'package:gifs_buscador/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
